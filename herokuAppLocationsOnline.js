5bd61f6474bba028cad2eaca0ae889f4677a7b6f
Brendan Lynch <45802003+brendanlynch9@users.noreply.github.com>
2020-01-31 10:54:25 -0600

    Add files via upload
    I have deployed live on to the internet Heroku apps as part of various bootcamps I attended. Here are a couple of their locations as examples of proof of knowledge.

---
+++ herokuAppLocationsOnline.js
@@ -0,0 +1,11 @@
+//Heroku sign up and to do list live online (I actually converted the to do list into an annual 
+//planner app too, for a pen making company! :)
+
+//Newsletter signup on heroku
+// https://peaceful-taiga-61800.herokuapp.com/
+
+// &
+
+// Heroku to do list
+// https://aqueous-caverns-16105.herokuapp.com/
+
